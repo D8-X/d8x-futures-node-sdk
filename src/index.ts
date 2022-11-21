87dc308ac2e652d5c81b6b9aeaa7958b82f2c1f3
m66260 <m66260@protonmail.com>
2022-11-21 09:38:11 -0500

    package setup

---
+++ src/index.ts
@@ -0,0 +1,32 @@
+import AccountTrade from "./accountTrade";
+import BrokerTool from "./brokerTool";
+import LiquidityProviderTool from "./liquidityProviderTool";
+import MarketData from "./marketData";
+import OrderReferrerTool from "./orderReferrerTool";
+import PerpetualDataHandler from "./perpetualDataHandler";
+import WriteAccessHandler from "./writeAccessHandler";
+// import {
+//   NodeSDKConfig,
+//   MarginAccount,
+//   CollaterlCCY,
+//   PoolStaticInfo,
+//   PerpetualStaticInfo,
+//   ExchangeInfo,
+//   PoolState,
+//   PerpetualState,
+//   Order,
+//   SmartContractOrder,
+// } from "./nodeSDKTypes";
+export * from "./nodeSDKTypes";
+export * from "./utils";
+export * from "./d8XMath";
+
+export {
+  AccountTrade,
+  BrokerTool,
+  LiquidityProviderTool,
+  MarketData,
+  OrderReferrerTool,
+  PerpetualDataHandler,
+  WriteAccessHandler,
+};
