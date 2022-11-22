3acb43b06f3f93ee2b3f72853911a446b5291920
m66260 <m66260@protonmail.com>
2022-11-22 14:33:06 -0500

    signature in broker tools + tests

--- src/brokerTool.ts
+++ src/brokerTool.ts
@@ -1,42 +1,74 @@
 import WriteAccessHandler from "./writeAccessHandler";
-import { NodeSDKConfig, Order, ORDER_MAX_DURATION_SEC, ZERO_ADDRESS } from "./nodeSDKTypes";
+import { NodeSDKConfig, Order, ORDER_MAX_DURATION_SEC, SmartContractOrder, ZERO_ADDRESS } from "./nodeSDKTypes";
 import PerpetualDataHandler from "./perpetualDataHandler";
 import { ABK64x64ToFloat } from "./d8XMath";
 import { text } from "stream/consumers";
-import { ethers } from "ethers";
+import { BigNumber, ethers } from "ethers";
 import AccountTrade from "./accountTrade";
 /**
  * BrokerTool.
- * Signature method for brokers
+ * Functions for brokers to determine fees, deposit lots, and sign-up traders.
  */
 export default class BrokerTool extends WriteAccessHandler {
   /**
    * Constructor
-   * @param config configuration
-   * @param privateKey private key of account that trades
+   * @param {NodeSDKConfig} config Configuration object.
+   * @param {string} privateKey Private key of a broker.
    */
   public constructor(config: NodeSDKConfig, privateKey: string) {
     super(config, privateKey);
   }
 
   /**
-   *
-   * @param symbol symbol of the form "ETH-USD-MATIC" or just "MATIC"
-   * @returns broker lot size in collateral currency, e.g. in MATIC for symbol ETH-USD-MATIC or MATIC
+   * Broker lot size for a given pool.
+   * @param {String} symbol Symbol of the form ETH-USD-MATIC or just MATIC.
+   * @returns {number} Broker lot size in collateral currency, e.g. in MATIC for symbol ETH-USD-MATIC or MATIC.
    */
-  public async getLotSize(symbol: string): Promise<number | undefined> {
+  public async getLotSize(symbol: string): Promise<number> {
     if (this.proxyContract == null) {
       throw Error("no proxy contract initialized. Use createProxyInstance().");
     }
     let poolId = PerpetualDataHandler._getPoolIdFromSymbol(symbol, this.poolStaticInfos);
     let pool = await this.proxyContract.getLiquidityPool(poolId);
-    let lot = pool?.fBrokerCollateralLotSize;
-    if (lot != undefined) {
-      lot = ABK64x64ToFloat(pool.fBrokerCollateralLotSize);
-    }
+    let lot = ABK64x64ToFloat(pool.fBrokerCollateralLotSize);
     return lot;
   }
 
+  /**
+   * Designation of this broker.
+   * @param {string} symbol Symbol of the form ETH-USD-MATIC or just MATIC.
+   * @returns {number} Number of lots purchased by this broker.
+   */
+  public async getBrokerDesignation(symbol: string): Promise<number> {
+    if (this.proxyContract == null || this.signer == null) {
+      throw Error("no proxy contract or wallet initialized. Use createProxyInstance().");
+    }
+    let poolId = PerpetualDataHandler._getPoolIdFromSymbol(symbol, this.poolStaticInfos);
+    let designation = await this.proxyContract.getBrokerDesignation(poolId, this.traderAddr);
+    return designation;
+  }
+
+  /**
+   * Determine the exchange fee based on lots purchased by this broker.
+   * @param {string} symbol Symbol of the form ETH-USD-MATIC or just MATIC
+   * @param {number} lots Optional, designation to use if different from this broker's.
+   * @returns {number} Fee based solely on this broker's designation, in decimals (i.e. 0.1% is 0.001).
+   */
+  public async getFeeForBrokerDesignation(symbol: string, lots: number = 0): Promise<number> {
+    if (this.proxyContract == null || this.signer == null) {
+      throw Error("no proxy contract or wallet initialized. Use createProxyInstance().");
+    }
+    let brokerDesignation = lots == 0 ? await this.getBrokerDesignation(symbol) : lots;
+    let feeTbps = await this.proxyContract.getFeeForBrokerDesignation(brokerDesignation);
+    return feeTbps / 100_000;
+  }
+
+  /**
+   * Deposit to a given pool.
+   * @param {string} symbol Symbol of the form ETH-USD-MATIC or just MATIC.
+   * @param {number} lots Number of lots to deposit into this pool.
+   * @returns Transaction object.
+   */
   public async brokerDepositToDefaultFund(symbol: string, lots: number): Promise<ethers.providers.TransactionResponse> {
     if (this.proxyContract == null || this.signer == null) {
       throw Error("no proxy contract or wallet initialized. Use createProxyInstance().");
@@ -46,9 +78,14 @@ export default class BrokerTool extends WriteAccessHandler {
     return tx;
   }
 
+  /**
+   * Determine the exchange fee based on volume traded under this broker.
+   * @param {string} symbol Symbol of the form ETH-USD-MATIC or just MATIC.
+   * @returns {number} Fee based solely on this broker's traded volume in the corresponding pool, in decimals (i.e. 0.1% is 0.001).
+   */
   public async getFeeForBrokerVolume(symbol: string): Promise<number | undefined> {
-    if (this.proxyContract == null) {
-      throw Error("no proxy contract initialized.");
+    if (this.proxyContract == null || this.signer == null) {
+      throw Error("no proxy contract or wallet initialized. Use createProxyInstance().");
     }
     let poolId = PerpetualDataHandler._getPoolIdFromSymbol(symbol, this.poolStaticInfos);
     let feeTbps = await this.proxyContract.getFeeForBrokerVolume(poolId, this.traderAddr);
@@ -56,100 +93,115 @@ export default class BrokerTool extends WriteAccessHandler {
   }
 
   /**
-   *
-   * @param symbol symbol of the form "ETH-USD-MATIC" or just "MATIC"
-   * @returns number of lots deposited by broker
+   * Determine exchange fee based on an order.
+   * @param {Order} order Order for which to determine the exchange fee, not necessarily signed by this broker.
+   * @param {string} traderAddr Address of the trader for whom to determine the fee.
+   * @returns {number} Fee in decimals (i.e. 0.1% is 0.001).
    */
-  public async getBrokerDesignation(symbol: string): Promise<number> {
-    if (this.proxyContract == null) {
-      throw Error("no proxy contract initialized.");
-    }
-    let poolId = PerpetualDataHandler._getPoolIdFromSymbol(symbol, this.poolStaticInfos);
-    let designation = await this.proxyContract.getBrokerDesignation(poolId, this.traderAddr);
-    return designation;
-  }
-
-  /**
-   * @param symbol symbol of the form "ETH-USD-MATIC" or just "MATIC"
-   * @param lots number of lots for which to get the fee. Defaults to this broker's current deposit if not specified
-   * @returns fee in decimals based on given number of lots
-   */
-  public async getFeeForBrokerDesignation(symbol: string, newLots: number = 0): Promise<number | undefined> {
-    if (this.proxyContract == null) {
-      throw Error("no proxy contract initialized.");
-    }
-    if (newLots < 0) {
-      throw Error("new lots must be a positive number.");
-    }
-    let lots = await this.getBrokerDesignation(symbol);
-    lots += newLots;
-    let feeTbps = await this.proxyContract.getFeeForBrokerDesignation(lots);
-    return feeTbps / 100_000;
-  }
-
-  /**
-   * @param order order for which to determine the trading fee
-   * @param traderAddr address of the trader for whom to determine the fee, defaults to lowest tier
-   * @returns fee in decimals (1% is 0.01)
-   */
-  public async determineExchangeFee(order: Order, traderAddr: string = ZERO_ADDRESS): Promise<number | undefined> {
-    if (this.proxyContract == null) {
-      throw Error("no proxy contract initialized.");
-    }
-    // broker does not need to enter address in the order if he's signed in
-    if (order.brokerAddr == undefined) {
-      if (this.signer == null) {
-        throw Error("no wallet initialized.");
-      }
-      order.brokerAddr = this.traderAddr;
+  public async determineExchangeFee(order: Order, traderAddr: string): Promise<number> {
+    if (this.proxyContract == null || this.signer == null) {
+      throw Error("no proxy contract or wallet initialized. Use createProxyInstance().");
     }
     let scOrder = AccountTrade.toSmartContractOrder(order, traderAddr, this.symbolToPerpStaticInfo);
     let feeTbps = await this.proxyContract.determineExchangeFee(scOrder);
     return feeTbps / 100_000;
   }
 
-  // public async sign(traderAddr: string, deadline: number, fee: number): Promise<string> {
-  //   /**
-  //    * structHash = keccak256(
-  //           abi.encode(
-  //               TRADE_BROKER_TYPEHASH,
-  //               _order.iPerpetualId,
-  //               _order.brokerFeeTbps,
-  //               _order.traderAddr,
-  //               _order.iDeadline
-  //           )
-  //       );
-  //       return structHash;
-  //    */
-  //   const NAME = "Perpetual Trade Manager";
-  //   const DOMAIN_TYPEHASH = ethers.utils.keccak256(
-  //     Buffer.from("EIP712Domain(string name,uint256 chainId,address verifyingContract)")
-  //   );
-  //   let abiCoder = ethers.utils.defaultAbiCoder;
-  //   let domainSeparator = ethers.utils.keccak256(
-  //     abiCoder.encode(
-  //       ["bytes32", "bytes32", "uint256", "address"],
-  //       [DOMAIN_TYPEHASH, ethers.utils.keccak256(Buffer.from(NAME)), chainId, proxyAddress]
-  //     )
-  //   );
-  //   let digest = ethers.utils.keccak256(
-  //     abiCoder.encode(["bytes32", "bytes32", "bool"], [domainSeparator, structHash, isNewOrder])
-  //   );
-  //   let digestBuffer = Buffer.from(digest.substring(2, digest.length), "hex");
-  //   return await signer.signMessage(digestBuffer);
-  // }
+  /**
+   * Fee that a trader would get if trading with this broker.
+   * @param {string} symbol Symbol of the form ETH-USD-MATC or just MATIC.
+   * @param {string} traderAddr Address of the trader.
+   * @returns {number} Exchange fee, in decimals (i.e. 0.1% is 0.001).
+   */
+  public async queryExchangeFee(symbol: string, traderAddr: string): Promise<number> {
+    if (this.proxyContract == null || this.signer == null) {
+      throw Error("no proxy contract or wallet initialized. Use createProxyInstance().");
+    }
+    let poolId = PerpetualDataHandler._getPoolIdFromSymbol(symbol, this.poolStaticInfos);
+    let feeTbps = await this.proxyContract.queryExchangeFee(poolId, traderAddr, this.traderAddr);
+    return feeTbps / 100_000;
+  }
 
-  /*
-  TODO:
-  - get lot size DONE
-  - purchase n lots:
-      brokerDepositToDefaultFund(symbol, amountLots) DONE
-  - fees:
-      getFeeForBrokerVolume DONE
-      determineExchangeFee(order) DONE
-      getBrokerDesignation DONE
-      getFeeForBrokerDesignation DONE
-  - get fee for trader and broker
-  - sign {trader address, deadline, broker fee}
-  */
+  /**
+   * Adds this broker's signature to an order so it can be submitted by an approved trader.
+   * @param {Order} order Order to sign.
+   * @param {string} traderAddr Address of trader submitting the order.
+   * @param {number} feeDecimals Fee that this broker is approving for the trader.
+   * @param {number} deadline Deadline for the order to be executed.
+   * @returns {Order} An order signed by this broker, which can be submitted directly with AccountTrade.order.
+   */
+  public async signOrder(order: Order, traderAddr: string, brokerFee: number, deadline: number): Promise<Order> {
+    order.brokerAddr = this.traderAddr;
+    order.brokerFeeTbps = brokerFee * 100_000;
+    order.deadline = deadline;
+    order.brokerSignature = await this.createSignatureForTrader(traderAddr, order.symbol, brokerFee, deadline);
+    return order;
+  }
+
+  /**
+   * Creates a signature that a trader can use to place orders with this broker.
+   * @param {string} traderAddr Address of the trader signing up with this broker.
+   * @param {string} symbol Perpetual that this trader will be trading, of the form ETH-USD-MATIC.
+   * @param {number} brokerFee Broker fee for this trader, in decimals (i.e. 0.1% is 0.001).
+   * @param {number} deadline Deadline for the order to be executed.
+   * @returns Broker signature approving this trader's fee, symbol, and deadline.
+   */
+  public async createSignatureForTrader(
+    traderAddr: string,
+    symbol: string,
+    brokerFee: number,
+    deadline: number
+  ): Promise<string> {
+    if (this.proxyContract == null || this.signer == null) {
+      throw Error("no proxy contract or wallet initialized. Use createProxyInstance().");
+    }
+    let perpetualId = PerpetualDataHandler.symbolToPerpetualId(symbol, this.symbolToPerpStaticInfo);
+    let iDeadline = BigNumber.from(deadline);
+    let brokerFeeTbps = 100_000 * brokerFee;
+    return await this._signOrder(
+      perpetualId,
+      brokerFeeTbps,
+      traderAddr,
+      iDeadline,
+      this.signer,
+      this.chainId,
+      this.proxyAddr
+    );
+  }
+
+  public async _signOrder(
+    iPerpetualId: number,
+    brokerFeeTbps: number,
+    traderAddr: string,
+    iDeadline: BigNumber,
+    signer: ethers.Wallet,
+    chainId: number,
+    proxyAddress: string
+  ): Promise<string> {
+    const NAME = "Perpetual Trade Manager";
+    const DOMAIN_TYPEHASH = ethers.utils.keccak256(
+      Buffer.from("EIP712Domain(string name,uint256 chainId,address verifyingContract)")
+    );
+    let abiCoder = ethers.utils.defaultAbiCoder;
+    let domainSeparator = ethers.utils.keccak256(
+      abiCoder.encode(
+        ["bytes32", "bytes32", "uint256", "address"],
+        [DOMAIN_TYPEHASH, ethers.utils.keccak256(Buffer.from(NAME)), chainId, proxyAddress]
+      )
+    );
+    //
+    const TRADE_BROKER_TYPEHASH = ethers.utils.keccak256(
+      Buffer.from("Order(uint24 iPerpetualId,uint16 brokerFeeTbps,address traderAddr,uint256 iDeadline)")
+    );
+    let structHash = ethers.utils.keccak256(
+      abiCoder.encode(
+        ["bytes32", "uint24", "uint16", "address", "uint256"],
+        [TRADE_BROKER_TYPEHASH, iPerpetualId, brokerFeeTbps, traderAddr, iDeadline]
+      )
+    );
+
+    let digest = ethers.utils.keccak256(abiCoder.encode(["bytes32", "bytes32"], [domainSeparator, structHash]));
+    let digestBuffer = Buffer.from(digest.substring(2, digest.length), "hex");
+    return await signer.signMessage(digestBuffer);
+  }
 }
--- test/broker.test.ts
+++ test/broker.test.ts
@@ -1,5 +1,5 @@
 import { ethers } from "ethers";
-import { NodeSDKConfig, ExchangeInfo, Order } from "../src/nodeSDKTypes";
+import { NodeSDKConfig, ExchangeInfo, Order, ZERO_ADDRESS } from "../src/nodeSDKTypes";
 import { ABK64x64ToFloat } from "../src/d8XMath";
 import PerpetualDataHandler from "../src/perpetualDataHandler";
 import AccountTrade from "../src/accountTrade";
@@ -30,42 +30,47 @@ describe("write and spoil gas and tokens", () => {
     await brokerTool.createProxyInstance();
   });
 
-  it("should get lot size", async () => {
+  it("should get lot size and some fees", async () => {
     let symbol = "MATIC";
     let lotSizeSC = await brokerTool.getLotSize(symbol);
-    if (lotSizeSC == undefined) {
-      console.log(`could not retrieve lot size for symbol ${symbol}`);
-      expect(false);
-    } else {
-      console.log(`lot size for ${symbol} pool is ${lotSizeSC} MATIC`);
-      lotSize = lotSizeSC;
+    console.log(`lot size for ${symbol} pool is ${lotSizeSC} MATIC`);
+    lotSize = lotSizeSC;
+    let designations = [1, 2, 5, 10, 15]; //, 20, 25, 40, 60, 100, 400, 600];
+    console.log("Some broker designations and fees:");
+    for (var k = 0; k < designations.length; k++) {
+      let lots = designations[k];
+      let fee = await brokerTool.getFeeForBrokerDesignation(symbol, lots);
+      console.log(`Exchange fee for ${lots} lots is ${100 * fee} %`);
     }
-  });
-
-  it("should set allowance to one lot", async () => {
-    let txHash = await brokerTool.setAllowance("MATIC", lotSize);
-    console.log(`set allowance tx hash = ${txHash}`);
   });
 
   it("should get broker designation [1]", async () => {
     let symbol = "ETH-USD-MATIC";
     let lots = await brokerTool.getBrokerDesignation(symbol);
-    console.log(`current broker designation: ${lots}`);
+    let fee = await brokerTool.getFeeForBrokerDesignation(symbol);
+    console.log(`current broker designation is ${lots} lots, with a fee of ${fee * 100} %`);
+  });
+
+  it("should set allowance to 10 lots", async () => {
+    // let txHash = await brokerTool.setAllowance("MATIC", 10 * lotSize);
+    // console.log(`set allowance tx hash = ${txHash}`);
   });
 
   it("should deposit a lot", async () => {
     let symbol = "MATIC";
     //** UNCOMENT TO ENABLE DEPOSIT
-    let tx = await brokerTool.brokerDepositToDefaultFund(symbol, 1);
-    console.log(`broker deposit transaction hash = ${tx.hash}`);
+    // let tx = await brokerTool.brokerDepositToDefaultFund(symbol, 7);
+    // console.log(`broker deposit transaction hash = ${tx.hash}`);
     // need to wait before next test to see lots increase
-    await tx.wait();
+    // await tx.wait();
     //*/
   });
 
   it("should get broker designation [2]", async () => {
     let symbol = "BTC-USD-MATIC";
     let lots = await brokerTool.getBrokerDesignation(symbol);
+    let fee = await brokerTool.getFeeForBrokerDesignation(symbol);
+    console.log(`current broker designation is ${lots} lots, with a fee of ${fee * 100} %`);
     console.log(`new broker designation: ${lots}`);
   });
 
@@ -75,7 +80,7 @@ describe("write and spoil gas and tokens", () => {
     console.log(`fee based on broker volume is ${10_000 * fee!} bps`);
   });
 
-  it("should determine total exchange fee, for unspecified trader", async () => {
+  it("should determine the exchange fee, for a trader that isn't signed up", async () => {
     let order: Order = {
       symbol: "MATIC-USD-MATIC",
       side: "BUY",
@@ -84,11 +89,18 @@ describe("write and spoil gas and tokens", () => {
       leverage: 2,
       timestamp: Date.now(),
     };
-    let fee = await brokerTool.determineExchangeFee(order);
-    console.log(`exchange fee for generic trader is ${10_000 * fee!} basis points`);
+    const myAddress = new ethers.Wallet(pk).address;
+    let fee = await brokerTool.determineExchangeFee(order, myAddress);
+    console.log(`exchange fee for an order with my address and no broker signature is ${10_000 * fee} basis points`);
   });
 
-  it("should determine total exchange fee, for a trader who is a token holder", async () => {
+  it("should query exchange fee based on broker and trader", async () => {
+    const myAddress = new ethers.Wallet(pk).address;
+    let fee = await brokerTool.queryExchangeFee("MATIC", myAddress);
+    console.log(`exchange fee for my address and this broker is ${10_000 * fee} basis points`);
+  });
+
+  it("should determine total exchange fee, for a trader who signed up [1]", async () => {
     let order: Order = {
       symbol: "ETH-USD-MATIC",
       side: "BUY",
@@ -98,7 +110,51 @@ describe("write and spoil gas and tokens", () => {
       timestamp: Date.now(),
     };
     const myAddress = new ethers.Wallet(pk).address;
-    let fee = await brokerTool.determineExchangeFee(order, myAddress);
-    console.log(`exchange fee for my wallet is ${10_000 * fee!} basis points`);
+    let brokerFee = 0.05;
+    let deadline = Date.now() + 10000;
+    let signedOrder = await brokerTool.signOrder(order, myAddress, brokerFee, deadline);
+    let fee = await brokerTool.determineExchangeFee(signedOrder, myAddress);
+    console.log(`exchange fee for a broker-signed order with my address is ${10_000 * fee} basis points`);
+  });
+
+  it("should trade a very large amount (to increase traded volume)", async () => {
+    //* UNCOMMENT TO ENABLE TRADING
+    // let accTrade = new AccountTrade(config, pk);
+    // await accTrade.createProxyInstance();
+    // let txHash = await accTrade.setAllowance("MATIC");
+    // let numOpenClose = 10;
+    // for (var k = 0; k < numOpenClose; k++) {
+    //   for (var j = 0; j < 2; j++) {
+    //     let amount = (-1) ** j * 10_000;
+    //     let order: Order = {
+    //       symbol: "MATIC-USD-MATIC",
+    //       side: "BUY",
+    //       type: "MARKET",
+    //       quantity: amount,
+    //       leverage: 10,
+    //       timestamp: Date.now(),
+    //     };
+    //     let tx = await accTrade.order(order);
+    //     console.log("trade transaction hash =", tx);
+    //   }
+    // }
+    //*/
+  });
+
+  it("should determine total exchange fee, for a trader who signed up [1]", async () => {
+    let order: Order = {
+      symbol: "ETH-USD-MATIC",
+      side: "BUY",
+      type: "MARKET",
+      quantity: 0.5,
+      leverage: 2,
+      timestamp: Date.now(),
+    };
+    const myAddress = ZERO_ADDRESS; //new ethers.Wallet(pk).address;
+    let brokerFee = 0.05;
+    let deadline = Date.now() + 10000;
+    let signedOrder = await brokerTool.signOrder(order, myAddress, brokerFee, deadline);
+    let fee = await brokerTool.determineExchangeFee(signedOrder, myAddress);
+    console.log(`exchange fee for a broker-signed order is ${10_000 * fee} basis points`);
   });
 });
