ad0986eeb40a1e18d28b3a6eadbfd6d4f9d64dd9
m66260 <m66260@protonmail.com>
2023-05-08 15:08:21 -0400

    fix version sorting

--- src/perpetualDataHandler.ts
+++ src/perpetualDataHandler.ts
@@ -954,7 +954,7 @@ export default class PerpetualDataHandler {
       return configFile[0];
     } else {
       if (version === undefined) {
-        configFile = configFile.sort((conf) => conf.version);
+        configFile = configFile.sort((conf) => -conf.version);
         return configFile[0];
       } else {
         return configFile.find((conf) => conf.version === version);
@@ -991,7 +991,7 @@ export default class PerpetualDataHandler {
       return configFile[0];
     } else {
       if (version === undefined) {
-        configFile = configFile.sort((conf) => conf.version);
+        configFile = configFile.sort((conf) => -conf.version);
         return configFile[0];
       } else {
         return configFile.find((conf) => conf.version === version);
