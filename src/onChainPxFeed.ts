bdd53aa087e115cbe5b8ca3a82864dbb8e3b0720
b1674927 <b1674927@protonmail.com>
2024-04-10 21:41:01 +0700

    change fetch approach to lazy

--- src/onChainPxFeed.ts
+++ src/onChainPxFeed.ts
@@ -2,6 +2,7 @@ import { Contract } from "@ethersproject/contracts";
 import { StaticJsonRpcProvider } from "@ethersproject/providers";
 import { decNToFloat } from "./d8XMath";
 import { BigNumber } from "ethers";
+import { sleep } from "./utils";
 
 /**
  * OnChainPxFeed: get a price from a chainlink-style oracle
@@ -10,12 +11,13 @@ export default class OnChainPxFeed {
   private lastResponseTs: number = 0;
   private lastPx: number = 0;
   private lastRpc: number = 0;
-  private delayMs = 2000;
+  private delayMs = 5000;
   public rpcs: string[];
   private provider: StaticJsonRpcProvider;
   //weETH/eETH on Arbitrum One 42161
   private ctrctAddr;
   private decimals;
+  private fetchInProgress = false;
   //static ctrctAddr = "0x119A190b510c9c0D5Ec301b60B2fE70A50356aE9";
   static abi = require("./abi/redStoneAbi.json");
 
@@ -27,29 +29,41 @@ export default class OnChainPxFeed {
     this.decimals = decimals;
   }
 
-  public async getPrice() {
-    if (Date.now() - this.lastResponseTs < this.delayMs) {
+  public async getPrice(): Promise<number> {
+    if (this.lastPx == undefined || Date.now() - this.lastResponseTs > 10 * 60 * 1000) {
+      await this.fetch(false);
       return this.lastPx;
     }
-    this.lastResponseTs = Date.now();
-    let hasErr = false;
-    for (let trial = 0; trial < this.rpcs.length; trial++) {
-      try {
-        let proxyContract = new Contract(this.ctrctAddr, OnChainPxFeed.abi, this.provider);
-        let data = await proxyContract.latestRoundData();
-        this.lastPx = decNToFloat(data.answer, this.decimals);
-        break;
-      } catch (err) {
-        console.log(`onChainPxSources error ${this.rpcs[this.lastRpc]}: retrying with other rpc`);
-        hasErr = true;
-      }
-      this.setRpc();
-    }
-    if (hasErr) {
-      console.log("onChainPxSources not successful returning last price");
-      return this.lastPx;
-    }
+    this.fetch(true);
     return this.lastPx;
+  }
+
+  private async fetch(delay: boolean) {
+    if (!this.fetchInProgress) {
+      this.fetchInProgress = true;
+      this.lastResponseTs = Date.now();
+      let hasErr = false;
+      for (let trial = 0; trial < this.rpcs.length; trial++) {
+        try {
+          let proxyContract = new Contract(this.ctrctAddr, OnChainPxFeed.abi, this.provider);
+          let data = await proxyContract.latestRoundData();
+          this.lastPx = decNToFloat(data.answer, this.decimals);
+          break;
+        } catch (err) {
+          console.log(`onChainPxSources error ${this.rpcs[this.lastRpc]}: retrying with other rpc`);
+          hasErr = true;
+        }
+        this.setRpc();
+      }
+      if (hasErr) {
+        console.log("onChainPxSources not successful");
+      }
+
+      if (delay) {
+        await sleep(this.delayMs);
+      }
+      this.fetchInProgress = false;
+    }
   }
 
   private setRpc() {
