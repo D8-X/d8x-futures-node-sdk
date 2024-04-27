70dabd3de21f2cb074a09836379ef1e92c5ea56f
b1674927 <b1674927@protonmail.com>
2024-04-27 08:58:53 +0400

    test

--- scripts/chaintest.ts
+++ scripts/chaintest.ts
@@ -2,21 +2,25 @@ import PerpetualDataHandler from "../src/perpetualDataHandler";
 
 import MarketData from "../src/marketData";
 import AccountTrade from "../src/accountTrade";
-
+import OrderExecutorTool from "../src/orderExecutorTool";
 import { Order, OrderResponse } from "../src/nodeSDKTypes";
 
 let pk: string = <string>process.env.PK;
 let RPC: string = <string>process.env.RPC;
 
 async function trade() {
-  let config = PerpetualDataHandler.readSDKConfig(196);
+  const sym = "WOKB-USD-WOKB"; //"ETH-USD-WEETH"; //
+  let config = PerpetualDataHandler.readSDKConfig(196); //42161); //
+  if (RPC != undefined) {
+    config.nodeURL = RPC;
+  }
   const accTrade = new AccountTrade(config, pk);
   await accTrade.createProxyInstance();
-  let tx = await accTrade.setAllowance("OKB-USD-OKB");
-  await tx.wait();
-  console.log("allowance:", tx.hash);
+  //let tx = await accTrade.setAllowance(sym);
+  //await tx.wait();
+  //console.log("allowance:", tx.hash);
   let order: Order = {
-    symbol: "OKB-USD-OKB",
+    symbol: sym,
     side: "SELL",
     type: "MARKET",
     quantity: 2,
@@ -24,7 +28,8 @@ async function trade() {
     executionTimestamp: Date.now() / 1000 - 10,
   };
   let resp: OrderResponse;
-  let orderId;
+  /*
+  let orderId = "";
   try {
     resp = await accTrade.order(order, undefined, { gasLimit: 800_000 });
     console.log("orderId =", resp.orderId);
@@ -33,8 +38,13 @@ async function trade() {
     orderId = resp.orderId;
   } catch (err) {
     console.log("Error=", err);
-  }
-  console.log("order submitted");
+  }*/
+  const orderId = "0xc1e068db1b15de6e27d77c79c3d8a508e049bab56ae0f829021b41f181b5a1f0";
+  console.log("order submitted id=", orderId);
+  const oex = new OrderExecutorTool(config, pk);
+  await oex.createProxyInstance();
+  let tx = await oex.executeOrder("BS-BS-BS", orderId);
+  console.log(tx);
 }
 
 async function main() {
@@ -52,4 +62,5 @@ async function main() {
 
   //await trade();
 }
-main();
+//main();
+trade();
